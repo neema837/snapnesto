31f893071c9a6b2a2ad3df5d4df248237254f74a
neema837 <83470727+neema837@users.noreply.github.com>
2024-08-21 11:42:14 +0530

    Update settings.py

--- facescan/settings.py
+++ facescan/settings.py
@@ -135,7 +135,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
